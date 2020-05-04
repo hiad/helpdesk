34f367eb5abfeb0efc4e7f9b180b618d13888eec
Hector Alcazar <hiad99@gmail.com>
2020-05-03 22:20:36 -0500

    Footer

--- src/pages/question.jsx
+++ src/pages/question.jsx
@@ -94,7 +94,7 @@ const Question = ({ location }) => {
         ({ node }) => node.title === title,
     ) || {};
 
-    const [{ node: filteredNode = {} }] = formattedNodeInfo || [{}];
+    const [{ node: filteredNode = {} }] = formattedNodeInfo || [{ node: {} }];
 
     const newCategoriesData = data.allContentfulQuestionType.edges || {};
     return (
